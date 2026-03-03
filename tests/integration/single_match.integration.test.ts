76f417a6f8961e12067953e210f3c19c4ddb6f2d
Andy Cave <andy.cave@nearform.com>
2026-03-03 14:34:57 +0000

    test: add failing test for walking skeleton

---
+++ tests/integration/single_match.integration.test.ts
@@ -0,0 +1,4 @@
+describe('given a single match between two teams from a json source when the match is processed', () => {
+  it.todo('stores both teams with updated ratings')
+  it.todo('writes a results file with teams sorted by rating')
+})
