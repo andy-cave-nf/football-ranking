6180ff6c8a61c89bcaa8d4232f3d71e421298c5f
Andy Cave <andy.cave@nearform.com>
2026-03-07 08:37:09 +0000

    test(ruleset): add test for 100 matches of alternating results

--- tests/unit/rulesets/trueskill.test.ts
+++ tests/unit/rulesets/trueskill.test.ts
@@ -108,9 +108,45 @@ describe('given two teams with a drawn result, when the result is processed', ()
   })
 })
 
+describe('given two teams that play many matches with alternating results, when each match is processed', () => {
+  let init: Ratings
+  let results: Result[]
+  let ratings: Ratings[]
+  beforeEach(() => {
+    ratings = []
+    init = {
+      home: { mu: 25, sigma: 25 / 3 },
+      away: { mu: 25, sigma: 25 / 3 },
+    };
+    results = Array.from({length:100}, (_,i) => ({
+      home: {id:'home-id',name:'team-1'},
+      away: {id: 'away-id',name:'team-2'},
+      homeWin: (i % 2) as 1|0,
+      date: new Date(),
+    }))
 
-describe.todo('given two teams that have played many matches, when a result is processed', () => {
-  it.todo('converges both sigma values to tau');
+    let newRating = init
+    for (const result of results) {
+      ratings.push(newRating)
+      newRating = ruleset.record(result,newRating)
+    }
+  })
+  it('converges both sigma values to tau', () => {
+    for (let i = 0; i < ratings.length-1; i++) {
+      const before = ratings[i] as Ratings
+      const after = ratings[i] as Ratings
+      expect(after.home.sigma - config.tau).toBeLessThanOrEqual(before.home.sigma-config.tau);
+      expect(after.away.sigma - config.tau).toBeLessThanOrEqual(before.away.sigma-config.tau);
+    }
+  });
+  it('decreases both sigma values', () => {
+    for (let i = 0; i < ratings.length-1; i++) {
+      const before = ratings[i] as Ratings
+      const after = ratings[i] as Ratings
+      expect(after.home.sigma).toBeLessThanOrEqual(before.home.sigma);
+      expect(after.away.sigma).toBeLessThanOrEqual(before.away.sigma);
+    }
+  })
 })
 
 describe.todo('given an upset result and a non-upset result between differently rated teams',() => {
