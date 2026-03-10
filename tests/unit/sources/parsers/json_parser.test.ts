190861aa6ec1033e9f1b4aa92c0fec95a6dc1853
Andy Cave <andy.cave@nearform.com>
2026-03-10 10:44:46 +0000

    test(parser): add tests for invalid data

--- tests/unit/sources/parsers/json_parser.test.ts
+++ tests/unit/sources/parsers/json_parser.test.ts
@@ -21,3 +21,31 @@ describe('given an empty invalid json file, when the file is read', () => {
 describe('given a json file with a fixture that has malformed score, when the file is read', () => {
   it.todo('raises a json parser error')
 })
+
+describe('given a json file with a fixture with an invalid date, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture with a missing result, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture with a missing home team id, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture with a missing away team id, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture with a missing home team name, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture with a missing away team name, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture with a missing date, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
