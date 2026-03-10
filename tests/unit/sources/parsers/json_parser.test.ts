3f37509404ce1f17bd59654b38243c0ad72ad2dc
Andy Cave <andy.cave@nearform.com>
2026-03-10 10:31:58 +0000

    test(parser): add tests for the json parser

---
+++ tests/unit/sources/parsers/json_parser.test.ts
@@ -0,0 +1,23 @@
+describe('given a json file containing a single match, when the file is read', () => {
+  it.todo('returns a single fixture')
+})
+
+describe('given a json file containing many matches, when the file is read', () => {
+  it.todo('returns all the matches')
+})
+
+describe('given a json file with an empty object, when the file is read', () => {
+  it.todo('returns an empty list')
+})
+
+describe('given a json file with an empty fixtures key, when the file is read', () => {
+  it.todo('returns an empty list')
+})
+
+describe('given an empty invalid json file, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
+
+describe('given a json file with a fixture that has malformed score, when the file is read', () => {
+  it.todo('raises a json parser error')
+})
