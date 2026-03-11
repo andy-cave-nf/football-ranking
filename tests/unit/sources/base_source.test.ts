6153f0e7f4490972ea5acefba9835a4f031f64f7
Andy Cave <andy.cave@nearform.com>
2026-03-11 20:42:32 +0000

    test(source): add tests for strict source dates

--- tests/unit/sources/base_source.test.ts
+++ tests/unit/sources/base_source.test.ts
@@ -1,4 +1,4 @@
-import { SafeSource, type Source, SourceError } from '../../../src/sources/base';
+import { SafeSource, type Source, SourceError, StrictSourceDates } from '../../../src/sources/base';
 import type { Result } from '../../../src/leagues/types';
 
 describe('Safe Source', () => {
@@ -43,11 +43,43 @@ describe('Safe Source', () => {
   })
 })
 
-describe.todo('Strict Source Dates', () => {
+describe('Strict Source Dates', () => {
+  let source: StrictSourceDates
   describe('given a source, when it is parsed with correctly ordered dates', () => {
-    it.todo('returns the data unchanged')
+    let origin: Source
+    let expected: Result[]
+    let actual: Result[]
+    beforeEach(async () => {
+      origin = {
+        async results(_start:Date, _end:Date):Promise<Result[]> {
+          return [
+            {
+              home: { id: 'id-1', name: 'team-1' },
+              away: { id: 'id-2', name: 'team-2' },
+              homeWin: 1,
+              date: new Date(),
+            },
+          ];
+        }
+      }
+      expected = await origin.results(new Date(), new Date())
+      source = new StrictSourceDates(origin)
+      actual = await source.results(new Date(), new Date())
+    })
+    it('returns the data unchanged', () => {
+      expect(actual).toEqual(expected)
+    })
   })
-  it.todo('a source error is raised if the end date is before the start date')
+  it('a source error is raised if the end date is before the start date', async () => {
+    const origin = {
+      async results(_start:Date, _end:Date) {
+        return []
+      }
+    }
+    source = new StrictSourceDates(origin)
+    await expect(() => source.results(new Date(2026,0,1), new Date(2000,0,1))).rejects.toThrowError(SourceError)
+
+  })
 })
 
 describe.todo('Sorted Source', () => {
