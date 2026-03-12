984bdc8c09791fd6a2236832716653b366ec8c34
Andy Cave <andy.cave@nearform.com>
2026-03-12 13:30:05 +0000

    refactor(utils): remove unused map extensions

--- src/utils.ts
+++ src/utils.ts
@@ -1,39 +1,3 @@
-export class ReadOnlyStrictMap<K, V> {
-  protected constructor(protected origin: ReadonlyMap<K, V>) {}
-  get(key: K): V | undefined {
-    return this.origin.get(key);
-  }
-  getOrThrow(id: K): V {
-    const value = this.origin.get(id);
-    if (value === undefined) {
-      throw new TeamMapError(`Cannot find id:${id}`);
-    }
-    return value;
-  }
-  has(id: K): boolean {
-    return this.origin.has(id);
-  }
-  values(): V[] {
-    return this.origin.values().toArray();
-  }
-  get size(): number {
-    return this.origin.size;
-  }
-}
-
-export class StrictMap<K, V> extends ReadOnlyStrictMap<K, V> {
-  public constructor(protected origin: Map<K, V>) {
-    super(origin);
-  }
-  set(id: K, value: V) {
-    this.origin.set(id, value);
-  }
-
-  toReadOnly(): ReadOnlyStrictMap<K, V> {
-    return new ReadOnlyStrictMap<K, V>(this.origin);
-  }
-}
-
 export class SanitizeMap<K, V> extends Map<K, V> {
   private readonly sanitize: (key: K) => K;
   constructor(sanitize: (key: K) => K, entries?: Iterable<[K, V]>) {
