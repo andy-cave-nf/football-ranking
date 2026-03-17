854e48e2d252324ec018252e5d380a68ba7c0853
Andy Cave <andy.cave@nearform.com>
2026-03-17 09:37:48 +0000

    test(leagues): pass some strict league record tests

--- tests/unit/leagues/base.test.ts
+++ tests/unit/leagues/base.test.ts
@@ -1,8 +1,9 @@
-import { StrictLeagueRecord, type League } from '../../../src/leagues/base';
+import { StrictLeagueRecord, type League, LeagueError } from '../../../src/leagues/base';
 import { defaultInMemoryLeague } from '../../utils';
 import { DefaultTeamMap } from '../../../src/leagues/team_maps';
 import type { Result, Team } from '../../../src/leagues/types';
 import type { SourceTeam } from '../../../src/sources/types';
+import { addDays } from 'date-fns';
 
 describe('Strict League Record', () => {
   let league: StrictLeagueRecord
@@ -50,20 +51,107 @@ describe('Strict League Record', () => {
       })
     })
   })
-  describe.todo('given a league with a single existing team', () => {
-    describe.todo('when a match involving one existing team and one new team, played after the existing teams last fixture date, is processed', () => {
-      it.todo('updates the existing team correctly')
-      it.todo('stores the new team correctly')
-      it.todo('adds only a single team to the league')
+  describe('given a league with a single existing team', () => {
+    let team: Team
+    let lastFixtureDate: Date
+    beforeEach(() => {
+      lastFixtureDate = new Date(2000,0,1)
+      team = {id:'team-id',name:'team-name',mu:10,sigma:0.5, lastFixtureDate}
+      origin = defaultInMemoryLeague({teamMap: new DefaultTeamMap(new Map<string,Team>([[team.id,team]]))})
+      league = new StrictLeagueRecord(origin);
     })
-    describe.todo('when a match involving the existing team and one new team, played before the existing teams last fixture date, is processed', () => {
-      it.todo('raises a League Error')
+    describe('when a match involving one existing team and one new team, played after the existing teams last fixture date, is processed', () => {
+      let newTeam: SourceTeam
+      let result: Result
+      let date: Date
+      beforeEach(async () => {
+        date = addDays(lastFixtureDate,1)
+        newTeam = {id: 'new-id', name: 'new-name'};
+        result = {
+          home: newTeam,
+          away: team,
+          homeWin: 1,
+          date,
+        }
+        league.record(result)
+      })
+      it('updates the existing team correctly', ()=> {
+        expect(league.teams.get(team.id)).toEqual({
+          ...team,
+          mu: team.mu+2,
+          sigma: team.sigma + 2,
+          lastFixtureDate: date,
+        })
+      })
+      it('stores the new team correctly', () => {
+        expect(league.teams.get(newTeam.id)).toEqual({
+          ...newTeam,
+          mu: 2,
+          sigma: 2,
+          lastFixtureDate: date,
+        })
+      })
+      it('adds only a single team to the league', () => {
+        expect(league.teams.size).toEqual(2)
+      })
     })
-    describe.todo('when a match involving two new teams, is processed', () => {
-      it.todo('stores the new home team correctly')
-      it.todo('stores the new away team correctly')
-      it.todo('leaves the existing team unchanged')
-      it.todo('adds only two teams to the league')
+    describe('when a match involving the existing team and one new team, played before the existing teams last fixture date, is processed', () => {
+      let newTeam: SourceTeam
+      let result: Result
+      let date: Date
+      beforeEach(async () => {
+        date = addDays(lastFixtureDate,-1)
+        newTeam = {id: 'new-id', name: 'new-name'};
+        result = {
+          home: team,
+          away: newTeam,
+          homeWin: 0,
+          date,
+        }
+      })
+      it('raises a League Error', () => {
+        expect(()=> league.record(result)).toThrow(LeagueError)
+      })
+    })
+    describe('when a match involving two new teams, is processed', () => {
+      let newHome: SourceTeam
+      let newAway: SourceTeam
+      let result: Result
+      let date: Date
+      beforeEach(async () => {
+        newHome = {id: 'new-home',name:'home-name'}
+        newAway = {id: 'new-away',name:'away-name'}
+        date = new Date(2000,0,1)
+        result = {
+          home: newHome,
+          away: newAway,
+          homeWin: 0.5,
+          date
+        }
+        league.record(result)
+      })
+      it('stores the new home team correctly', () => {
+        expect(league.teams.get(newHome.id)).toEqual({
+          ...newHome,
+          mu: 2,
+          sigma: 2,
+          lastFixtureDate: date
+        })
+      })
+      it('stores the new away team correctly', () => {
+        expect(league.teams.get(newAway.id)).toEqual({
+          ...newAway,
+          mu: 3,
+          sigma: 3,
+          lastFixtureDate: date
+        })
+      })
+      it('leaves the existing team unchanged', ()=> {
+        expect(league.teams.get(team.id)).toEqual(team)
+      })
+      it('adds only two teams to the league', () => {
+        expect(league.teams.size).toEqual(3)
+      })
     })
     describe.todo('when a match involving two new teams, played before the existing teams last fixture date, is processed', () => {
       it.todo('does not raise a League Error')
