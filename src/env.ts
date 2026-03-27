6895378c9f4b0b700f4296721a7f21cec9fe0a78
C1419775 <andy.cave@virginmediao2.co.uk>
2026-03-27 11:58:20 +0000

    ci: require the environment keys

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,18 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+function required(key:string) {
+  const value = process.env[key];
+  if (!value) throw new Error(`Environment variable ${key} not found`);
+  return value;
+}
+
 interface Env {
   API_KEY: string;
   API_URL: string;
 }
 
 export const apiEnv: Env = {
-  API_KEY: process.env.API_KEY || '',
-  API_URL: process.env.API_URL || '',
+  API_KEY: required('API_KEY'),
+  API_URL: required('API_URL'),
 };
