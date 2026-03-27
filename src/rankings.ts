d4667d7e33ca23f309da23bed259ac32606e47e4
C1419775 <andy.cave@virginmediao2.co.uk>
2026-03-27 05:31:54 +0000

    feat(rankings): remove printing functionality

--- src/rankings.ts
+++ src/rankings.ts
@@ -2,11 +2,9 @@ import type { Page } from './pages/pages';
 import type { Result } from './leagues/types';
 import type { Source } from './sources/base';
 import type { League } from './leagues/base';
-import type { Ruleset } from './rulesets/base';
 
 export interface Rankings {
   run(from: Date, to: Date): Promise<void>;
-  print(page: Page): Promise<void>;
 }
 
 export class DefaultRankings implements Rankings {
@@ -18,32 +16,6 @@ export class DefaultRankings implements Rankings {
     const results: Result[] = await this.source.results(from, to);
     results.forEach((match) => this.league.record(match));
   }
-
-  async print(page: Page): Promise<void> {
-    await page.print(this.league.teams.values());
-  }
-}
-
-abstract class StrictRankings implements Rankings {
-  protected constructor(protected origin: Rankings) {}
-  async run(from: Date, to: Date): Promise<void> {
-    await this.origin.run(from, to);
-  }
-  async print(page: Page): Promise<void> {
-    await this.origin.print(page);
-  }
-}
-
-export class RankingsWithStrictDates extends StrictRankings {
-  constructor(origin: Rankings) {
-    super(origin);
-  }
-  async run(from: Date, to: Date): Promise<void> {
-    if (from > to) {
-      throw new RankingError(`${from} is later than ${to}`);
-    }
-    await this.origin.run(from, to);
-  }
 }
 
 export class SafeRankings implements Rankings {
@@ -53,13 +25,6 @@ export class SafeRankings implements Rankings {
       await this.origin.run(from, to);
     } catch (error) {
       throw new RankingError('Error while running ranking', { cause: error });
-    }
-  }
-  async print(page: Page): Promise<void> {
-    try {
-      await this.origin.print(page);
-    } catch (error) {
-      throw new RankingError('Error while printing to page', { cause: error });
     }
   }
 }
--- tests/unit/ranking.test.ts
+++ tests/unit/ranking.test.ts
@@ -6,12 +6,6 @@ import type { Source } from '../../src/sources/base';
 import type { League } from '../../src/leagues/base';
 import { DefaultTeamMap} from '../../src/leagues/team_maps';
 
-// let rankings: Rankings;
-// let league: League;
-// let source: Source;
-// let dates: { start: Date; end: Date };
-// let results: Result[];
-
 describe.only('Default Rankings', () => {
   let rankings: DefaultRankings;
   let source: Source;
@@ -22,6 +16,7 @@ describe.only('Default Rankings', () => {
       this.actual.push(result)
     }
     get teams(){return new DefaultTeamMap(new Map<string,Team>())}
+    standings(){return []}
   }
   describe('given a source with a single match and an empty league', () => {
     beforeEach(async() => {
@@ -123,95 +118,3 @@ describe.only('Default Rankings', () => {
     })
   })
 })
-
-
-//
-// beforeEach(async () => {
-//   dates = { start: new Date(2000, 0, 1), end: new Date(2001, 0, 1) };
-//   results = [
-//     {
-//       home: { id: '1', name: 'team-1' },
-//       away: { id: '2', name: 'team-3' },
-//       homeWin: 1,
-//       date: new Date(dates.start),
-//     },
-//     {
-//       home: { id: '2', name: 'team-2' },
-//       away: { id: '3', name: 'team-3' },
-//       homeWin: 0.5,
-//       date: new Date(dates.start),
-//     },
-//     {
-//       home: { id: '1', name: 'team-1' },
-//       away: { id: '3', name: 'team-3' },
-//       homeWin: 0,
-//       date: new Date(dates.start),
-//     },
-//   ];
-//
-//   league = new FakeLeague(25,25/3);
-//   source = new FakeSource(results);
-//   rankings = new DefaultRankings(league, source);
-// });
-//
-// describe('Ranking makes correct calls on running', async () => {
-//   beforeEach(async () => {
-//     await rankings.run(dates.start, dates.end);
-//   });
-//
-//   it('tests matches are called from the source', async () => {
-//     expect(source.results).toBeCalledWith(dates.start, dates.end);
-//   });
-//
-//   it('tests that the matches are recorded by the league', async () => {
-//     results.forEach((result) => {
-//       expect(league.record).toBeCalledWith(result);
-//     });
-//   });
-// });
-//
-// describe('test that printing rankings call pages', async () => {
-//   let fakePage: Page;
-//   let spy: Mock<() => ReadOnlyTeamMap<number | string, Team>>;
-//   beforeEach(async () => {
-//     spy = vi.spyOn(FakeLeague.prototype, 'teams', 'get');
-//     fakePage = new FakePage();
-//     await rankings.print(fakePage);
-//   });
-//   afterEach(async () => {
-//     vi.restoreAllMocks();
-//     vi.clearAllMocks();
-//   });
-//   it('tests that teams are called from leagues', async () => {
-//     expect(spy).toBeCalledTimes(1);
-//   });
-//
-//   it('tests print is called once', async () => {
-//     expect(fakePage.print).toBeCalledWith(league.teams.values());
-//   });
-// });
-//
-// describe('test strict rankings throws errors', async () => {
-//   let strictRankings: Rankings;
-//   beforeEach(async () => {
-//     strictRankings = new RankingsWithStrictDates(rankings);
-//   });
-//   it('tests an error is raised if the dates are incompatible', async () => {
-//     await expect(
-//       strictRankings.run(new Date(2020, 1, 1), new Date(1970, 1, 1))
-//     ).rejects.toThrowError(RankingError);
-//   });
-// });
-//
-// describe('tests error handling of safe rankings', async () => {
-//   let safeRankings: Rankings;
-//   beforeEach(async () => {
-//     safeRankings = new SafeRankings(new ErroredRanking());
-//   });
-//   it('tests run raises a ranking error', async () => {
-//     await expect(safeRankings.run(dates.start, dates.end)).rejects.toThrowError(RankingError);
-//   });
-//   it('tests page raises a ranking error', async () => {
-//     await expect(safeRankings.print(new FakePage())).rejects.toThrowError(RankingError);
-//   });
-// });
