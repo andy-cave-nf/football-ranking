dd69459a2ce732b70a725f719106d4f379a47636
C1419775 <andy.cave@virginmediao2.co.uk>
2026-03-27 16:03:40 +0000

    test: add integration test for premier league match

--- tests/integration/single_match.integration.test.ts
+++ tests/integration/single_match.integration.test.ts
@@ -8,7 +8,16 @@ import { DefaultTeamMap } from '../../src/leagues/team_maps';
 import type { Team } from '../../src/leagues/types';
 import { SafeJson } from '../../src/sources/parsers/base';
 import { DefaultJsonFixtures, ValidatedJsonScores, ValidatedJsonShape } from '../../src/sources/parsers/json_parse';
-import type { League } from '../../src/leagues/base';
+import  { type League, SafeLeague } from '../../src/leagues/base';
+import {
+  SafeSource,
+  SortedSource,
+  type Source,
+  StrictSourceDates,
+  StrictSourceIds,
+  UniqueResultsSource,
+} from '../../src/sources/base';
+import { ApiSource } from '../../src/sources/api_source';
 
 
 describe('given a single match between two teams from a json source', () => {
@@ -23,10 +32,12 @@ describe('given a single match between two teams from a json source', () => {
       beta: 25/6,
       drawRate: 0.25
     }
-    league = new InMemoryLeague(
-      new DefaultTeamMap(new Map<string,Team>()),
-      new DefaultTrueSkill(config)
-    )
+    league = new SafeLeague(
+        new InMemoryLeague(
+        new DefaultTeamMap(new Map<string,Team>()),
+        new DefaultTrueSkill(config)
+        )
+      )
     const filepath = path.resolve(process.cwd(), 'tests', 'fixtures', 'json_source', 'single_match.json')
     ranking =
       new DefaultRankings(
@@ -45,6 +56,48 @@ describe('given a single match between two teams from a json source', () => {
   describe('when the match is processed', () => {
     beforeEach(async () => {
       await ranking.run(new Date(2025, 8, 16), new Date(2026, 5, 25));
+    })
+    it('stores both teams with ratings', () => {
+      expect(league.standings()).toHaveLength(2)
+    })
+  })
+})
+
+describe('given a single premier league match between two teams from an api source', () => {
+  let rankings: Rankings
+  let config: TrueSkillConfig
+  let league: League
+  let source: Source
+  beforeEach(async () => {
+    config = {
+      mu0: 25,
+      sigma0: 25 / 3,
+      tau: 25 / 300,
+      beta: 25 / 6,
+      drawRate: 0.25,
+    };
+    league = new SafeLeague(
+        new InMemoryLeague(
+        new DefaultTeamMap(new Map<string, Team>()),
+        new DefaultTrueSkill(config)
+      )
+    );
+    source = new StrictSourceDates(
+      new StrictSourceIds(
+        new UniqueResultsSource(
+          new SortedSource(
+            new SafeSource(
+              new ApiSource(["39"])
+            )
+          )
+        )
+      )
+    )
+    rankings = new DefaultRankings(league, source)
+  })
+  describe('when the match is processed', () => {
+    beforeEach(async () => {
+      await rankings.run(new Date(2023,0,12),new Date(2023,0,12))
     })
     it('stores both teams with ratings', () => {
       expect(league.standings()).toHaveLength(2)
