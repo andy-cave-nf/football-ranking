42fe577d95701d6f98dfda15f8627a94f9aba083
C1419775 <andy.cave@virginmediao2.co.uk>
2026-03-30 13:03:52 +0100

    test(source): add todo tests for api queries

---
+++ tests/unit/sources/api_source/query.test.ts
@@ -0,0 +1,66 @@
+
+describe('Default Api Query', () => {
+  describe('given a query for a premier league api source', () => {
+    describe.todo('when the date 2024-01-01 is processed', () => {
+      it.todo('returns a query for the 2023 season')
+      it.todo('returns a query for the 2024 season')
+      it.todo('does not return queries for other seasons')
+    })
+    describe.todo('when the date range 2024-01-01 and 2024-12-31 is processed', () => {
+      it.todo('returns a query for the 2023 season')
+      it.todo('returns a query for the 2024 season')
+      it.todo('does not return queries for other seasons')
+    })
+    describe.todo('when the date range 2024-01-01 and 2026-01-01 is processed', () => {
+      it.todo('returns a query for the 2023 season');
+      it.todo('returns a query for the 2024 season');
+      it.todo('returns a query for the 2025 season');
+      it.todo('returns a query for the 2026 season');
+      it.todo('returns a query for the season for the year between the start and end date');
+      it.todo('does not return queries for other seasons');
+    });
+    describe.todo('when the date range 2024-01-01 and 2025-01-01 is processed', () => {
+      it.todo('returns a query for the 2023 season')
+      it.todo('returns a query for the 2024 season')
+      it.todo('returns a query for the 2025 season')
+      it.todo('returns a query for the season for the year between the start and end date')
+      it.todo('does not return queries for other seasons')
+    })
+  })
+  describe('given a query for a premier league and fa cup api source', () => {
+    describe('when the date 2024-01-01 is processed', () => {
+      it.todo('returns a query for the premier league 2023 season')
+      it.todo('returns a query for the premier league 2024 season')
+      it.todo('returns a query for the fa cup 2023 season')
+      it.todo('returns a query for the fa cup 2024 season')
+      it.todo('does not return queries for other seasons')
+    })
+    describe('when the date range 2024-01-01 and 2024-12-31 is processed', () => {
+      it.todo('returns a query for the premier league 2023 season')
+      it.todo('returns a query for the premier league 2024 season')
+      it.todo('returns a query for the fa cup 2023 season')
+      it.todo('returns a query for the fa cup 2024 season')
+      it.todo('does not return queries for other seasons')
+    })
+    describe('when the date range 2024-01-01 and 2025-01-01 is processed', () => {
+      it.todo('returns a query for the premier league 2023 season')
+      it.todo('returns a query for the premier league 2024 season')
+      it.todo('returns a query for the premier league 2025 season')
+      it.todo('returns a query for the fa cup 2023 season')
+      it.todo('returns a query for the fa cup 2024 season')
+      it.todo('returns a query for the fa cup 2025 season')
+      it.todo('does not return queries for other seasons')
+    })
+    describe('when the date range 2024-01-01 and 2026-01-01 is processed', () => {
+      it.todo('returns a query for the premier league 2023 season');
+      it.todo('returns a query for the premier league 2024 season');
+      it.todo('returns a query for the premier league 2025 season');
+      it.todo('returns a query for the premier league 2026 season');
+      it.todo('returns a query for the fa cup 2023 season');
+      it.todo('returns a query for the fa cup 2024 season');
+      it.todo('returns a query for the fa cup 2025 season');
+      it.todo('returns a query for the fa cup 2026 season');
+      it.todo('does not return queries for other seasons');
+    });
+  })
+})
