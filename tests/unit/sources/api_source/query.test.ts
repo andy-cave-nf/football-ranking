fc6a8c50ee85dc1222ebb3c83a14db5b1662b37b
C1419775 <andy.cave@virginmediao2.co.uk>
2026-03-30 14:44:06 +0100

    test(source): remove rogue import

--- tests/unit/sources/api_source/query.test.ts
+++ tests/unit/sources/api_source/query.test.ts
@@ -1,10 +1,30 @@
-
 describe('Default Api Query', () => {
+  let query : DefaultApiQuery
+  let startDate: Date;
+  let endDate: Date;
   describe('given a query for a premier league api source', () => {
-    describe.todo('when the date 2024-01-01 is processed', () => {
-      it.todo('returns a query for the 2023 season')
-      it.todo('returns a query for the 2024 season')
-      it.todo('does not return queries for other seasons')
+    beforeEach(async () => {
+      query = new DefaultApiQuery(["39"])
+    })
+    describe('when the date 2024-01-01 is processed', () => {
+      let actual: string
+      beforeEach(async () => {
+        startDate = new Date('2024-01-01');
+        endDate = startDate
+        actual = query.query(startDate, endDate)
+      })
+      it('returns a query for the 2023 season', () => {
+        const expected = 'https://v3.football.api-sports.io/fixtures?from=2024-01-01&to=2024-01-01&season=2023&league=39'
+        expect(actual).toContain(expected);
+      })
+      it('returns a query for the 2024 season', () => {
+        const expected =
+          'https://v3.football.api-sports.io/fixtures?from=2024-01-01&to=2024-01-01&season=2024&league=39';
+        expect(actual).toContain(expected)
+      })
+      it('does not return queries for other seasons', () => {
+        expect(actual).toHaveLength(2)
+      })
     })
     describe.todo('when the date range 2024-01-01 and 2024-12-31 is processed', () => {
       it.todo('returns a query for the 2023 season')
