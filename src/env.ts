9c6ab351e4f860961c5932c8640cdd01737a7706
C1419775 <andy.cave@virginmediao2.co.uk>
2026-04-01 10:39:32 +0100

    chore: add endpoint to api as environment variable

--- src/env.ts
+++ src/env.ts
@@ -10,9 +10,11 @@ function required(key:string) {
 interface Env {
   API_KEY: string;
   API_URL: string;
+  API_ENDPOINT: string
 }
 
 export const apiEnv: Env = {
   API_KEY: required('API_KEY'),
   API_URL: required('API_URL'),
+  API_ENDPOINT: required('API_ENDPOINT'),
 };
